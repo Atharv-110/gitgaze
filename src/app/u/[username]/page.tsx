e7938aa032d8478adee96d5a7fa759fdc48ccb31
Atharv-110 <atharvavani110@gmail.com>
2025-12-12 04:08:46 +0530

    fixed the build issue

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,13 +1,17 @@
+import { Metadata, ResolvingMetadata } from "next";
 import BentoComponent from "@/components/bento-component";
-import { Metadata } from "next";
 
 type Props = {
   params: Promise<{ username: string }>;
 };
 
-export async function generateMetadata(props: Props): Promise<Metadata> {
-  const { username } = await props.params;
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { username } = await params;
   const lowercasedUsername = username.toLowerCase();
+
   return {
     title: lowercasedUsername,
     description: `Discover ${lowercasedUsername}’s GitHub statistics and activity in a personalized, visual, and share-ready dashboard — highlighting their achievements, contributions, and developer journey.`,
@@ -17,8 +21,9 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   };
 }
 
-const UserPage = async ({ params }: { params: { username: string } }) => {
-  const { username } = params;
+const UserPage = async ({ params }: Props) => {
+  const { username } = await params;
+
   return (
     <section className="mt-16 xl:max-w-screen-xl w-full mx-auto">
       <BentoComponent username={username} />
